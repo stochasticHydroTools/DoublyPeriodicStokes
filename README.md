25339050b6b92d5b0d6ce6000ffda0b18d6a69e1
RaulPPealez <raul.perez@uam.es>
2021-06-08 18:37:27 +0200

    Update to include torque in DP

--- README.md
+++ README.md
@@ -17,12 +17,25 @@ Run make to compile, you might have to adapt it to your particular system before
   
 ### Python interface
 
-A python interface is provided that allows to compute the product between the mobility tensor (from a group of Gaussian sources) and a vector (i.e the force) in different geometries, mainly:  
+A python interface is provided that allows to compute the hydrodynamic displacements for a group of positions with forces and torques acting on them in different geometries, mainly:  
+
 	* Doubly periodic (either with no walls, a bottom wall or a slit channel)  
 	* Triply periodic (using force coupling method)  
+
+Note that for now, FCM cannot deal with torques, just forces.
+
 This interface requires [pybind11](https://github.com/pybind/pybind11) to compile, which is included as a submodule and will be automatically downloaded if this repo is cloned recursively (see "About this repo" above).  
 In order to use it you must compile the python wrappers using make (doing ```make python``` or ```make all``` here will also compile the python library).  
 A file called uammd.*.so will be created and then "import uammd" can be used inside python. Notice that you might have to customize python\_interface/Makefile for your particular system.  
 See python_interface/dpstokes.py for a usage example.  
 Once compiled you can use "help(uammd)" for additional usage information.  
+
+## About the spreading/interpolation kernels in the python interface
+
+The doubly periodic options will use the ES kernel (called BM in UAMMD), in this case the tolerance parameter will be ignored and the support must be specified.  
+The gw parameter will always be ignored (it comes from when a Gaussian was used).  
+
+The triply periodic mode (FCM) will use a Gaussian, the tolerance parameter can be specified instead of the support (which will be autocomputed from it).  
+
+Other kernels can be used, but the interface does not allow them for the moment, since the specific kernel parameters are still a work in progress.
 
--- python_interface/uammd_wrapper.cu
+++ python_interface/uammd_wrapper.cu
@@ -1,10 +1,11 @@
 /* Raul P. Pelaez 2021. Doubly Periodic Stokes python bindings
-   Allows to call the DPStokes module from python to compute the product between the mobility tensor and a list forces acting on a group of Gaussian sources.
+   Allows to call the DPStokes module from python to compute the product between the mobility tensor and a list forces and torques acting on a group of positions.
    For additional info use:
    import uammd
    help(uammd)
 
 */
+#include "Integrator/BDHI/DoublyPeriodic/StokesSlab/utils.cuh"
 #include<pybind11/pybind11.h>
 #include<pybind11/numpy.h>
 #include <uammd.cuh>
@@ -16,17 +17,21 @@ namespace py = pybind11;
 using uammd::BDHI::FCM;
 using DPStokesSlab = uammd::DPStokesSlab_ns::DPStokes;
 using uammd::DPStokesSlab_ns::WallMode;
-
+using uammd::System;
 using real = uammd::real;
 struct PyParameters{
-  int nxy, nz;
+  //The number of cells in each direction
+  //If -1, they will be autocomputed from the tolerance if possible (DP cannot do it, FCM can)
+  int nxy = -1;
+  int nz = -1;
   real dt;
   real viscosity;
   real Lxy;
   real H;
+  //Tolerance will be ignored in DP mode
   real tolerance = 1e-7;
-  real gw;
-  int support = -1; //-1 means auto compute from tolerance
+  real gw; //Gaussian width, unused with the BM kernel and in TP
+  int support = -1; //-1 means auto compute from tolerance if possible (FCM can do this)
   //Can be either none, bottom, slit or periodic
   std::string mode;
 };
@@ -46,9 +51,9 @@ struct Real4ToReal3{
 
 FCM::Parameters createFCMParameters(PyParameters pypar){
   FCM::Parameters par;
-  par.temperature = 0;
-  par.viscosity = pypar.viscosity;  
-  par.tolerance = 1e-5;
+  par.temperature = 0; //FCM can compute fluctuations, but they are turned off here
+  par.viscosity = pypar.viscosity;
+  par.tolerance = pypar.tolerance;
   par.box = uammd::Box({pypar.Lxy, pypar.Lxy, pypar.H});
   par.cells = {pypar.nxy, pypar.nxy, pypar.nz};
   return par;
@@ -103,26 +108,45 @@ struct UAMMD {
     CudaSafeCall(cudaStreamCreate(&st));
   }
 
-  void Mdot(py::array_t<real> h_pos, py::array_t<real> h_forces, py::array_t<real> h_MF){
+  void Mdot(py::array_t<real> h_pos, py::array_t<real> h_forces, py::array_t<real> h_torques,
+	    py::array_t<real> h_MF,
+	    py::array_t<real> h_MT){
     tmp.resize(numberParticles);
+    bool useTorque = h_torques.size() != 0;
     {
       auto pos = pd->getPos(uammd::access::gpu, uammd::access::write);
       auto force = pd->getForce(uammd::access::gpu, uammd::access::write);
-      std::copy((uammd::real3*)h_pos.data(), (uammd::real3*)h_pos.data() + numberParticles, tmp.begin());
+      thrust::copy((uammd::real3*)h_pos.data(), (uammd::real3*)h_pos.data() + numberParticles, tmp.begin());
       thrust::transform(thrust::cuda::par, tmp.begin(), tmp.end(), pos.begin(), Real3ToReal4());
-      std::copy((uammd::real3*)h_forces.data(), (uammd::real3*)h_forces.data() + numberParticles, tmp.begin());
+      thrust::copy((uammd::real3*)h_forces.data(), (uammd::real3*)h_forces.data() + numberParticles, tmp.begin());
       thrust::transform(thrust::cuda::par, tmp.begin(), tmp.end(), force.begin(), Real3ToReal4());
+      if(useTorque){
+	auto torque = pd->getTorque(uammd::access::gpu, uammd::access::write);
+        thrust::copy((uammd::real3*)h_torques.data(), (uammd::real3*)h_torques.data() + numberParticles, tmp.begin());
+	thrust::transform(thrust::cuda::par, tmp.begin(), tmp.end(), torque.begin(), Real3ToReal4());
+      }
     }
-    auto force = pd->getForce(uammd::access::gpu, uammd::access::read);    
+    auto force = pd->getForce(uammd::access::gpu, uammd::access::read);
+    auto pos = pd->getPos(uammd::access::gpu, uammd::access::read);
     if(fcm){
+      if(h_torques.size() != 0){
+    	System::log<System::EXCEPTION>("Cannot process torques in triply periodic mode");
+    	throw std::runtime_error("Invalid mode");
+      }
       auto tmp_ptr = thrust::raw_pointer_cast(tmp.data());
       fcm->Mdot(tmp_ptr, force.raw(), 0);
       thrust::copy(tmp.begin(), tmp.end(), (uammd::real3*)h_MF.mutable_data());
     }
-    else if(dpstokes){
-      auto pos = pd->getPos(uammd::access::gpu, uammd::access::read);
-      auto MF = dpstokes->Mdot(pos.raw(), force.raw(), numberParticles, st);
-      auto MF_real3 = thrust::make_transform_iterator(MF.begin(), Real4ToReal3());
+    if(dpstokes){
+      auto torque = pd->getTorqueIfAllocated(uammd::access::gpu, uammd::access::read);
+      auto d_torques_ptr = useTorque?torque.raw():nullptr;
+      //mob is a tuple containing MF and MT. The mobilities for translational and rotational contributions
+      auto mob = dpstokes->Mdot(pos.raw(), force.raw(), d_torques_ptr, numberParticles, st);
+      if(mob.second.size()){
+	auto MT_real3 = thrust::make_transform_iterator(mob.second.begin(), Real4ToReal3());
+	thrust::copy(MT_real3, MT_real3 + numberParticles, (uammd::real3*)h_MT.mutable_data());
+      }
+      auto MF_real3 = thrust::make_transform_iterator(mob.first.begin(), Real4ToReal3());
       thrust::copy(MF_real3, MF_real3 + numberParticles, (uammd::real3*)h_MF.mutable_data());
     }
   }
@@ -132,8 +156,6 @@ struct UAMMD {
     cudaStreamDestroy(st);
   }
 };
-
-
 
 using namespace pybind11::literals;
 
@@ -153,8 +175,9 @@ PYBIND11_MODULE(uammd, m) {
   m.doc() = "UAMMD DPStokes Python interface";
   py::class_<UAMMD>(m, "DPStokes").
     def(py::init<PyParameters, int>(),"Parameters"_a, "numberParticles"_a).
-    def("Mdot", &UAMMD::Mdot, "Computes the product of the Mobility tensor with a provided array",
-	"positions"_a,"forces"_a,"result"_a);
+    def("Mdot", &UAMMD::Mdot, "Computes the product of the Mobility tensor with the provided forces and torques. If torques are not present, they are assumed to be zero and angular displacements will not be computed",
+	"positions"_a,"forces"_a, "torques"_a = py::array_t<real>(),
+	"velocities"_a, "angularVelocities"_a = py::array_t<real>());
   
   py::class_<PyParameters>(m, "StokesParameters").
     def(py::init([](uammd::real viscosity,
