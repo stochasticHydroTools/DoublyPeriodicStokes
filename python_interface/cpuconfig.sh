ac3371003a4441a7d5db5f0274b3f747d6650b3c
snatesh <sachin.natesh@gmail.com>
2021-09-15 15:35:59 -0600

    add choice to pin threads in cpuconfig.sh

--- python_interface/cpuconfig.sh
+++ python_interface/cpuconfig.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-DPSTOKES_ROOT=
-DPSTOKES_INSTALL=
-CPU=
+DPSTOKES_ROOT=/home/sachin/Research/NYU/ParticleSuspension/DoublyPeriodicStokes
+DPSTOKES_INSTALL=/home/sachin/Research/NYU/ParticleSuspension/DoublyPeriodicStokes/python_interface/lib
+CPU=Intel
 
 #################### BEGIN USER EDIT ####################################
 
 # number of threads for OpenMP
-num_threads=10
+num_threads=6
 # let the shell use the maximum amount of stack memory
 ulimit -s unlimited
 # set mem for thread stack
 export OMP_STACKSIZE=256m
+pin_threads=false
 
 # thread pinning settings
-CPU=GNU
-if [ "$CPU" == "Intel" ]; then
+if [ "$CPU" == "Intel" -a "$pin_threads" == "true" ]; then
   export MKL_THREADING_LAYER=sequential
-  export KMP_AFFINITY="verbose,proclist=[0,1,2,3,4,5,6,7,8,9],explicit"
-else
-  export OMP_PLACES="{0}:10:1"
+  export KMP_AFFINITY="verbose,proclist=[0,1,2,3,4,5,6],explicit"
+elif [ "$pin_threads" == "true" ]; then
+  export OMP_PLACES="{0}:6:1"
   export OMP_PROC_BIND=true
   export OMP_DISPLAY_ENV=true
 fi
